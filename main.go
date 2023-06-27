0bae1b08518f86e6a245b068a16ceb95c1407f29
zahra-ehghaghi <z.ehghaghi@gmail.com>
2023-06-27 22:11:06 +0330

    feat: add golang project

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
-import "net/http"
-import "fmt"
-import "time"
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-const timeout = 3* time.Second
-func main(){
-http.HandleFunc("/hello", hello)
-server :=  &http.Server{
-	Addr: ":8090",
-	ReadHeaderTimeout : timeout,
-}
-if err := server.ListenAndServe(); err != nil {
-	panic(err)
-}
+const timeout = 3 * time.Second
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	server := &http.Server{
+		Addr:              ":8090",
+		ReadHeaderTimeout: timeout,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
 
-func hello(w http.ResponseWriter, _ *http.Request){
-fmt.Fprintf(w,"Hello from zahra")
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello from zahra\n")
 
 }
